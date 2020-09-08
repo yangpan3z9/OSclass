506e1cc36832464ac612bbe501a16d024b675d97
yangpan3z9 <theyanger333@gmail.com>
2020-09-08 13:27:03 -0500

    add pathdir.cpp

---
+++ pathdir.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <dirent.h>
+using namespace std;
+
+int depthfirstapply(char *path, int pathfun(char #path1));
+
+int sizepathfun(char *path);
+
+int main() {
+
+
+
+
+}
+
+
+
